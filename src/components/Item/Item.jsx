d350389f3aa8498d0b56fe07f5384b20697f1c02
Sebastian Zuviria <szuviria@gmail.com>
2023-10-19 02:22:49 -0300

    prueba

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,3 @@
-import { Link, useNavigate } from "react-router-dom"
-
 const Item = ({ id, name, img, price }) => {
 
 
@@ -15,7 +13,7 @@ const Item = ({ id, name, img, price }) => {
             <h3>{name}</h3>
             <img src={img} style={{ width: 100}}/>
             <p>Precio: u$s {price}</p>
-            <Link to={`/detail/${id}`} >Ver Detalle</Link>
+            <button>Ver Detalle</button>
             {/* <button onClick={() => navigate('/detail')}>Detalle</button> */}
         </div>
     )
