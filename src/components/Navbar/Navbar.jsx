16b40ce4ee87cc5ed8ad000927eb598f6baeae7f
Sebastian Zuviria <szuviria@gmail.com>
2023-10-19 02:19:33 -0300

    prueba

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import classes from './Navbar.module.css'
 import { useEffect, useState } from 'react'
 import CartWidget from "../CartWidget/CartWidget"
-import { NavLink, useNavigate } from 'react-router-dom'
 import { db } from '../../services/firebase/firebaseConfig'
 import { getDocs, collection, query, orderBy } from 'firebase/firestore'
 
 const Navbar = () => {
     const [categories, setCategories] = useState([])
-    const navigate = useNavigate()
+
 
     useEffect(() => {
         const categoriesRef = query(collection(db, 'categories'), orderBy('order', 'asc'))
@@ -26,11 +25,11 @@ const Navbar = () => {
 
     return (
         <nav className={classes.container}>
-            <h1 onClick={() => navigate('/')}>Ecommerce</h1>
+            <h1>Ecommerce</h1>
             <section>
                 {
                     categories.map(cat => (
-                        <NavLink key={cat.id} to={`/category/${cat.slug}`} className={({ isActive }) => isActive ? classes.active : classes.inactive }>{cat.name}</NavLink>
+                        <button key={cat.id} className={ classes.inactive}>{cat.name}</button>
                     ))
                 }
             </section>
